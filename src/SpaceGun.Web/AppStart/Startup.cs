194a06c27592ab67043fc724ff981dca7e00e149
Lawrence Dine <lawrence.dine@moov2.com>
2015-01-31 16:54:32 +0000

    Add Owin and Nancy with a hello world page.

---
+++ src/SpaceGun.Web/AppStart/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin.Extensions;
+using Owin;
+
+namespace SpaceGun.Web.AppStart
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy((options) =>
+            {
+
+            })
+            .UseStageMarker(PipelineStage.MapHandler);
+        }
+    }
+}
---
+++ src/SpaceGun.Web/Modules/MainModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpaceGun.Web.Modules
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ =>
+            {
+                return "Hello world";
+            };
+        }
+    }
+}
